55390e35bf4cac4bdbb1b66d28eb29bd4b42be58
Clement <clement.ramos@laplateforme.io>
2023-01-20 09:52:41 +0100

    Regular project done, without bonuses

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from tkinter import *
 from tkinter import ttk
+import json
 
 #My sized fonts
-MAIN_CALC_FONT = ("Arial", 40, "bold")
-SUB_CALC_FONT = ("Arial", 16)
-BUTTONS_FONT = ("Arial", 20, "bold")
-DEFAULT_FONT_STYLE = ("Arial", 20)
+DEFAULT_FONT_STYLE = ("Arial", 14)
 
 #Colors that I use
 WHITE = "#FFFFFF"
@@ -38,6 +36,7 @@ def convert():
     except ValueError: 
         print("The only available inputs are numbers")
 
+
 def add_to_historic(validate_amount_float, first_currency, last_currency, result):
     historic.append((validate_amount_float, first_currency, result, last_currency))
     print(historic)
@@ -47,23 +46,19 @@ def add_to_historic(validate_amount_float, first_currency, last_currency, result
 def update_historic_display():
     sub.delete(0, END)
     for element in historic:
-        #string_insert = historic[element][0] + historic[element][1] + " worth " + historic[element][3] + historic[element][2]
         sub.insert(END, element)
-        
+
 
 def clear_historic():
     global historic
     historic = []
     update_historic_display()
-#     
-#     for validate_amount_float, first_currency, last_currency in historic:
-#         sub.insert(END, validate_amount_float + first_currency + " worth " + result + last_currency)
-#     sub.config(yscrollcommand=scrollbar.set)
-#     scrollbar.config(command=sub.yview)
-    
+
+
 """
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Driver Code ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ GUI Config ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
+
 # create a GUI window
 gui = Tk()
 # set the configuration of GUI window
@@ -75,48 +70,57 @@ gui.title("Currency Converter")      # set the title of GUI window
 result = StringVar()
 historic = []
 
+#Create the list with all the currencies and ther ratio (USD to "the currency")
+file = open('data.json')
+rate_from_USD_to=json.load(file)
+currencies = [i for i in rate_from_USD_to]
+
+"""
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Historic ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+
 #Displaying my historic
 sub = Listbox(gui)
 sub.pack(side = TOP, fill=BOTH)
 scrollbar = Scrollbar(sub)
 scrollbar.pack(side=RIGHT, fill='both')
 
-labelChoix = Label(gui, text = "Select an amount and your currencies!", pady=10)
-labelChoix.pack()
+labelChoix = Label(gui, text = "Select an amount and your currencies!",font=DEFAULT_FONT_STYLE)
+labelChoix.pack(fill=X, ipady=5)
+
+"""
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Amount and ComboBox ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
 
 amount = Entry(gui)     #Text input box
-amount.pack(side = TOP)
-
-#Create the list with all the currencies and ther ratio (USD to "the currency")
-rate_from_USD_to={"USD":1, "EUR":1.08, "GBP":0.8171, "JPY":128.525, "AED":3.67244}
-currencies = [i for i in rate_from_USD_to]
-    
+amount.pack(side = TOP, ipady=10)
+        
 #Create the 2 combo boxes to choose currencies
 starting_currency = ttk.Combobox(gui, values=currencies)
-starting_currency.pack(side = TOP)
+starting_currency.pack(side = TOP, ipady=5)
 starting_currency.current(0)
 starting_currency.bind("<<ComboboxSelected>>")
 starting_currency.pack()
 
 final_currency = ttk.Combobox(gui, values=currencies)
-final_currency.pack(side = TOP)
+final_currency.pack(side = TOP, ipady=5)
 final_currency.current(0)
 final_currency.bind("<<ComboboxSelected>>")
 
 #Convert button who call convert()
-convert_button = Button(gui, text="Convert !", command=convert)
+convert_button = Button(gui, text="Convert !", command=convert, pady=10, padx=5)
 convert_button.pack(side=TOP)
 
 #Displaying the result
-label_result_title = Label(gui, text = "Result :")
-label_result_title.pack()
+label_result_title = Label(gui, text = "Result :",font=DEFAULT_FONT_STYLE)
+label_result_title.pack(fill=X)
 
 label_result = Label(gui, textvariable=result)
-label_result.pack(side=TOP)
+label_result.pack(side=TOP, fill=X)
 
 #Convert button who call convert()
 convert_button = Button(gui, text="Clear historic", command=clear_historic)
-convert_button.pack(side=TOP)
+convert_button.pack(ipadx=5, ipady=5)
 
 #Display GUI
 gui.mainloop()
